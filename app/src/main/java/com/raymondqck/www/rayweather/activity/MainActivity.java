6562f3ce57ac7da6482737f80bb9aa204d5a6c9b
RaymondCHAN <raymondcqk@163.com>
2016-05-21 20:28:10 +0800

    packages test

--- app/src/main/java/com/raymondqck/www/rayweather/activity/MainActivity.java
+++ app/src/main/java/com/raymondqck/www/rayweather/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.raymondqck.www.rayweather;
+package com.raymondqck.www.rayweather.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.raymondqck.www.rayweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
